61d6bf0cfd3db977dd515584a193a60af9af8f67
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-05-23 20:31:12 +0700

    aaaaaaaaaaaaaaaaaaa

---
+++ code/views/template/footer.php
@@ -0,0 +1,10 @@
+<?php
+// views/template/footer.php
+?>
+    <!-- </div> <footer>
+        <p style="text-align: center; margin-top: 20px; padding: 20px; background: #333; color: #fff;">
+            
+        </p>
+    </footer>
+    </body>
+</html> -->
